a09b056402549db0eacd0f1316ab45bff4a154ec
Lee <coduf2@gmail.com>
2025-03-25 14:20:13 +0900

    Steer Page UI 개발

--- App/WpfApp/SteerPage.xaml.cs
+++ App/WpfApp/SteerPage.xaml.cs
@@ -1,28 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp
 {
-    /// <summary>
-    /// SteerPage.xaml에 대한 상호 작용 논리
-    /// </summary>
     public partial class SteerPage : Page
     {
-        public SteerPage()
+        private string _brake;
+
+        public SteerPage(string brake)
         {
             InitializeComponent();
+            _brake = brake;
+
+            Loaded += SteerPage_Loaded;
+        }
+
+        private void SteerPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            TitleTextBlock.Text = $"{_brake} 브레이크 선택됨 — 스티어링 선택";
+
+            List<string> steerOptions = new List<string> { "Bosch", "HYUNDAI" };
+
+            foreach (var steer in steerOptions)
+            {
+                var button = new Button
+                {
+                    Content = steer,
+                    Height = 50,
+                    Margin = new Thickness(0, 0, 0, 15),
+                    FontSize = 18
+                };
+
+                button.Click += (s, e2) =>
+                {
+                    MessageBox.Show($"{steer} 스티어링 선택됨");
+                    // TODO: 결과 페이지로 이동 가능
+                };
+
+                SteeringButtonContainer.Children.Add(button);
+            }
         }
     }
 }
