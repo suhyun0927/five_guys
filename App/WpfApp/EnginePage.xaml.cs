98226349444ed714165a573b80d25ae85f8a7c76
yunlolee <dbsfh56@gmail.com>
2025-03-26 13:46:19 +0900

    Added logos

--- App/WpfApp/EnginePage.xaml.cs
+++ App/WpfApp/EnginePage.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Navigation;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace WpfApp
 {
@@ -27,12 +28,47 @@ namespace WpfApp
             {
                 var btn = new Button
                 {
-                    Content = label,
-                    Height = 50,
-                    FontSize = 18,
+                    Height = 120, // 버튼 높이를 늘려서 이미지와 텍스트가 잘 보이게 함
+                    Width = 200,
                     Margin = new Thickness(0, 0, 0, 15),
-                    Tag = value
+                    FontSize = 18,
+                    Background = Brushes.White,
+                    Tag = value,
+                    VerticalAlignment = VerticalAlignment.Center, // 버튼 내에서 수직 중앙 정렬
+                    HorizontalAlignment = HorizontalAlignment.Center // 버튼 내에서 수평 중앙 정렬
                 };
+
+                // 이미지 추가
+                Image engineImage = new Image
+                {
+                    Source = new BitmapImage(new Uri($"pack://application:,,,/Assets/{label.ToLower()}.png")), // 이미지 경로
+                    Width = 50,
+                    Height = 50,
+                    Margin = new Thickness(5)
+                };
+
+                // 텍스트 추가
+                TextBlock textBlock = new TextBlock
+                {
+                    Text = label,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    FontSize = 18,
+                    FontWeight = FontWeights.Bold,
+                    TextAlignment = TextAlignment.Center  // 텍스트 가운데 정렬
+                };
+
+                // StackPanel을 사용하여 수직 배치
+                StackPanel stackPanel = new StackPanel
+                {
+                    Orientation = Orientation.Vertical,
+                    VerticalAlignment = VerticalAlignment.Center, // StackPanel 내에서 수직 중앙 정렬
+                    HorizontalAlignment = HorizontalAlignment.Center // StackPanel 내에서 수평 중앙 정렬
+                };
+                stackPanel.Children.Add(engineImage);
+                stackPanel.Children.Add(textBlock);
+
+                // 버튼에 StackPanel 추가
+                btn.Content = stackPanel;
 
                 btn.Click += (s, e) =>
                 {
